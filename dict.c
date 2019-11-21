b722584cfd6f9e56b64c3632059d8282c54e76c9
jae_hyeok <wolfchoi09@naver.com>
2019-11-21 19:43:34 +0900

    test

--- dict.c
+++ dict.c
@@ -4,6 +4,7 @@
 #include <time.h>
 #include <unistd.h>
 #define PRINT_STR(x) printf(#x" : %s\n", x)
+//test
 typedef struct word_struct{
 	char eng[16]; //영어단어 15자 + '\0'
 	char kor[93]; //한글단어 30자 + 공백 최대 2개 + '\0' (한글은 한글자에 utf-8기준 3바이트)
