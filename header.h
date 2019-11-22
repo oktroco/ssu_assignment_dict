aaedd18f61a0341b32e7ca89a8b436581086e8ad
oktroco <andyoh12@naver.com>
2019-11-22 11:20:01 +0900

    main.c, manage.c를 분리, 새파일 추가하기 기능 완성

--- header.h
+++ header.h
@@ -10,4 +10,13 @@ typedef struct word_struct{
 	char kor[93]; //한글단어 30자 + 공백 최대 2개 + '\0' (한글은 한글자에 utf-8기준 3바이트)
 	struct word_struct *next; //자기 참조를 위한 포인터변수
 } word_struct;
+int make_struct(FILE *dict, word_struct *word);
+FILE *read_file(void);
+void choose_dic(word_struct *word);
+void make_word_array(word_struct **word_array, word_struct *word);
+int compare_word(const void *a, const void *b);
+void hang_man(void);
+void manage_init(void);
+void manage_add(void);
+void input_words(FILE *f);
 #endif
---
+++ manage.c
@@ -0,0 +1,77 @@
+#include "header.h"
+void manage_init(void){
+	//word_struct *word = (word_struct *)malloc(sizeof(word_struct)); //첫 구조체word
+	//choose_dic(word); //파일선택 후 자기참조구조체 생성
+ 	//word_struct *word_array[cnt];
+	//make_word_array(word_array, word); //포인터배열 생성
+
+	system("clear");
+	char num = 0;
+	while(num != 5){
+		fprintf(stderr, ">>영어 단어 암기 프로그램 : 단어장 관리<<\n");		//메뉴 출력
+		fprintf(stderr, "1. 새 파일 추가하기\t2. 새 단어 추가하기\n");
+		fprintf(stderr, "3. 단어장 보기\t 4. 단어 파일 목록보기\n");
+		fprintf(stderr, "5. 단어장 관리 종료\n");
+		printf("\n\n번호를 입력하세요 : ");
+		scanf("%hhd", &num);
+		while(getchar() != '\n'); //입력버퍼 비우기
+
+		if (num == 1)
+			manage_add();	//단어장관리
+		else if (num == 2)
+			;
+		else if (num == 3)
+			;	//플래시카드
+		else if (num == 4)
+			;	//단어맞추기
+		else if (num == 5)
+			;
+		else
+			fprintf(stderr, "잘못된 입력\n");	//1~5 사이의 입력이 아닐때 오류메세지 출력
+		putchar('\n');
+	}
+}
+
+void manage_add(void){
+	FILE *f = fopen("dic.list", "r");
+	if(f == NULL){
+		fprintf(stderr, "dic.list를 찾을 수 없습니다.\n");
+		return;
+	}
+	char last_name[20], tmp_name[20];
+	int last_file_num = 1;
+	while((fscanf(f, "%s\n", tmp_name)) != EOF)
+		strcpy(last_name, tmp_name);
+	sscanf(last_name, "%d", &last_file_num);
+	sprintf(last_name, "%d", last_file_num + 1);
+	strcat(last_name, ".dic");
+	FILE *new_f = fopen(last_name, "w");
+	PRINT_STR(last_name);
+	input_words(new_f);
+	fclose(new_f);
+	fclose(f);
+	FILE *f_append = fopen("dic.list", "a");
+	fprintf(f_append, "%s\n", last_name);
+	fclose(f_append);
+	printf("새 단어장 %s가 추가되었습니다.\n\n", last_name);
+}
+
+void input_words(FILE *f){
+	char eng[16], kor[93];
+	int word_cnt = 0;
+	while(word_cnt < 20){
+		printf("최대 20단어까지 입력가능하며, 단어입력을 끝내려면 .add를 입력해주세요");
+		printf("남은 단어 입력 수 : %d\n", 20 - word_cnt);
+		printf("영어 단어를 입력하세요(최대 15자) : \n");
+		scanf("%s", eng);
+		while(getchar() != '\n');								//(추가) 개행문자 혹은 나머지 입력버퍼 비우기
+		if(!strcmp(eng, ".add"))
+			break;
+		printf("한글 뜻을 입력하세요(뜻은 최대 3개, 띄어쓰기로 구분, 모든 한글 뜻 합쳐서 30자까지 : \n");
+		scanf("%[^\n]", kor);
+		while(getchar() != '\n');								//(추가) 개행문자 혹은 나머지 입력버퍼 비우기
+		fprintf(f, "%s %s\n", eng, kor);
+		printf("%s : %s \n추가완료\n\n", eng, kor);
+		word_cnt++;
+	}
+}
