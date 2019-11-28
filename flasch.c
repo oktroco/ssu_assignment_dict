03651a94a2c5c0c2fbf8ddfc8dc6c8135ae43209
jae_hyeok <wolfchoi09@naver.com>
2019-11-28 19:40:38 +0900

    flasch 수정 및 for문 문법수정

--- flasch.c
+++ flasch.c
@@ -1,21 +1,21 @@
 void flasch(void){
+	extern int cnt;
 	printf(">>영어 단어 암기 프로그램 : 플래쉬카드<<");
-	int time, how;
+	int time, how, i;
 	word_struct *word=(word_struct*)malloc(sizeof(word_struct));
 	printf("속도(초) : ");
 	scanf("%d", &time);
 	choose_dic(word);
 	word_struct *word_arry[cnt];
-	make_word_arry(word_arry, word);
+	make_word_array(word_arry, word);
 	printf("출력방식(알파벳 순서대로 : 1, 무작위 : 2)");
 	scanf("%d", &how);
 	if(how==1)
 		qsort(word_arry, cnt, sizeof(word_struct*), compare_word);
-	time*=1000;
-	for(int i=0; i<cnt; i++){
-		printf("%s",word_arry[i]->eng);
+	for(i=0; i<cnt; i++){
+		printf("%s ",word_arry[i]->eng);
 		sleep(time);
-		printf("%s", word_arry[i]->kor);
+		printf("%s\n", word_arry[i]->kor);
 		sleep(time);
 	}
 }
